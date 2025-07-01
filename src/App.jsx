aed94fbcc4e8df37b3f1961b2319d5a7bf0c3dcb
FuthifoXx <futhifoxmaseko@gmail.com>
2025-07-01 14:50:00 +0200

    fetched tours and rendered them

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,42 @@
+import { useEffect, useState } from 'react';
+import Loading from './components/Loading';
+import Tours from './components/Tours';
+
 const url = 'https://www.course-api.com/react-tours-project';
 
 const App = () => {
-  return <h2>Tours Starter</h2>;
+  const [isLoading, setIsLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+
+  const fetchData = async () => {
+    setIsLoading(true);
+    try {
+      const resp = await fetch(url);
+      const tours = await resp.json();
+      setTours(tours);
+    } catch (error) {
+      console.log(error);
+    }
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  if (isLoading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
+  //TODO
+
+  return (
+    <main>
+      <Tours tours={tours} />
+    </main>
+  );
 };
 export default App;
